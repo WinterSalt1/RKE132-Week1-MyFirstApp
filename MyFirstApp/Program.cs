fb0669fafece57360114346c8432218dcedc5da1
Mikk Soomre <mikk@soomre.com>
2022-09-16 19:38:17 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code
+
+// Rakendus küsib kasutajalt sisestada tema nime
+// Rakendus tervitab kasutajat nime pidi
+
+Console.WriteLine("Enter Your Name: "); // Output
+
+// string - sõne
+
+string userName = Console.ReadLine(); // Input
+
+// string interpolation
+
+Console.WriteLine($"Hello {userName}" + " !"); // Output
